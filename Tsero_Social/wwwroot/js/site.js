fadd63a63e68768fb984442f45b9e710635ad0cc
Tseretel1 <giorgitsereteli541@gmail.com>
2024-05-01 20:55:26 +0400

    Burger Menu

--- Tsero_Social/wwwroot/js/site.js
+++ Tsero_Social/wwwroot/js/site.js
@@ -1,4 +1,46 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿
+function BurgerMenuShow() {
+    var HeaderShow = document.getElementById("HeaderBurgerContainer");
+    var Menu = document.getElementById("HeaderBurger");
+    var Navigators = document.querySelectorAll(".NavBurger");
+    HeaderShow.style.transition = "0.3s all ease-in-out";
+    Menu.style.transition = "0.3s all ease-in-out";
 
-// Write your JavaScript code.
+    HeaderShow.style.opacity = "0"
+    HeaderShow.style.display = "flex";
+    setTimeout(() => {
+        Menu.style.width = "50%";
+        HeaderShow.style.opacity = "1"
+    }, 100);
+    setTimeout(() => {
+        Navigators.forEach(function (nav) {
+            nav.style.opacity = "1";
+        });
+    }, 200);
+}
+
+var BurgerShowButton = document.getElementById("BurgerButton");
+BurgerShowButton.addEventListener("click", BurgerMenuShow);
+
+
+function BurgerMenuDelete() {
+    var HeaderShow = document.getElementById("HeaderBurgerContainer");
+    var Menu = document.getElementById("HeaderBurger");
+    var Navigators = document.querySelectorAll(".NavBurger");
+    HeaderShow.style.transition = "0.3s all ease-in-out";
+    Menu.style.transition = "0.3s all ease-in-out";
+
+    HeaderShow.style.opacity = "0"
+    Menu.style.width = "0%";
+    setTimeout(() => {
+        HeaderShow.style.display = "none";
+    }, 300);
+    setTimeout(() => {
+        Navigators.forEach(function (nav) {
+            nav.style.opacity = "0";
+        });
+    }, 100);
+}
+
+var BurgerShowButtonn = document.getElementById("BurgerButton2");
+BurgerShowButtonn.addEventListener("click", BurgerMenuDelete);
